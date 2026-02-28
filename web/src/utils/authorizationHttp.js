11c755b4939e56e0fb8663a7091087899f289059
gmslymhn <gmslymhn@gmail.com>
2026-02-28 14:25:08 +0800

    修复token过期后不会自动跳转到登录页面的问题

--- web/src/utils/authorizationHttp.js
+++ web/src/utils/authorizationHttp.js
@@ -1,5 +1,7 @@
 // src/utils/authorizationHttp.js
-import axios from 'axios'; // 改为直接从 axios 库导入
+import axios from 'axios';
+import router from '@/router'; // 需要引入路由实例
+import { ElMessage } from 'element-plus'
 
 const authorizationHttp = axios.create({
   baseURL: 'http://8.137.83.173:8081',
@@ -18,9 +20,25 @@ authorizationHttp.interceptors.request.use(config => {
 
 // 响应拦截器
 authorizationHttp.interceptors.response.use(response => {
-  return response.data;
+  const data = response.data;
+
+  // 检查返回的实体类中的状态码
+  if (data && data.code === 401) {
+    // 清除 token
+    sessionStorage.removeItem('token');
+
+
+    //可以添加一个提示
+    ElMessage.error(data.message || '请先登录！');
+    // 跳转到登录页面
+    router.push('/login');
+
+  }
+
+  // 其他成功的响应正常返回
+  return data;
 }, error => {
-  // 错误处理逻辑
+
   return Promise.reject(error);
 });
 
